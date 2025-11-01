c767c0f7238cbcf3a216b3c65f637322e9268c12
temptationkentos57 <kreetlannoo@hotmail.com>
2025-11-01 10:40:46 +0100

    Update server message to use English.

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-// Các route sẽ được định nghĩa ở đây
+// Routes will be defined here
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server đã chạy trên cổng ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
